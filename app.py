50751ec25659f47116461e3da084127b8f05fcee
hennadesai <tur39413@temple.edu>
2026-04-12 18:57:25 -0400

    Update app.py

--- app.py
+++ app.py
@@ -18,7 +18,7 @@ fallback=pd.DataFrame({
 @st.cache_data
 def load_who_data():
   try:
-    df = pd.read_csv("who.csv.csv.xlsx")
+    df = pd.read_excel("who.csv.csv.xlsx")
 
     #Debug: see columns
     st.write("WHO columns:", df.columns)
@@ -48,7 +48,7 @@ def load_who_data():
 @st.cache_data
 def load_cdc_data():
   try:
-    df = pd.read_csv("cdc.csv.csv.xlsx")
+    df = pd.read_excel("cdc.csv.csv.xlsx")
 
     #Debug
     st.write("CDC columns:", df.columns)
