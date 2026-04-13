b54472e4205b06353db2addccbf44ac01543252b
hennadesai <tur39413@temple.edu>
2026-04-13 17:06:46 -0400

    Update app.py

--- app.py
+++ app.py
@@ -53,7 +53,7 @@ def load_who_data():
 @st.cache_data
 def load_cdc_data():
   try:
-    df = pd.read_excel("cdc.csv.xlsx", encoding="openpyxl")
+    df = pd.read_excel("cdc.xlsx", encoding="openpyxl")
 
     #Debug
     st.write("CDC columns:", df.columns)
